73c273165c038fa0756d5394341b006a23279bc2
sebarbour <sebarbour@hotmail.com>
2016-07-06 09:24:35 +0200

    Add files via upload

--- JStest.js
+++ JStest.js
@@ -3,20 +3,10 @@ var FORUM = parent.document.getElementById("o_c1318029145");
 console.log(FORUM);
 }
 function findForum(){
-	
 }
-
-	var EVENTLNR = parent.document.getElementById("chubaka");
-	console.log(EVENTLNR);
-(function checkChubaka(){	
-	if(EVENTLNR){
-		EVENTLNR.addEventListener("click", function(){
-		console.log("Event fired");
-		});
-		console.log("Event listener added");
-		return; 
-	}else{
-		setTimeout(checkChubaka,100);
-	}
-)();
+var EVENTLNR = parent.document.getElementById("chubaka");
+console.log(EVENTLNR);
+EVENTLNR.addEventListener("click", function(){
+	console.log("Event fired");
+});
 
